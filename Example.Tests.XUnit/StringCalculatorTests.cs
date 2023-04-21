94c818e19e00be7ca56605dd15ce116456f137e9
Vakhushti Metreveli <vakhushti.metreveli@gmail.com>
2023-04-21 18:19:27 +0400

    Initial commit

---
+++ Example.Tests.XUnit/StringCalculatorTests.cs
@@ -0,0 +1,133 @@
+﻿using StringCalculatorKata;
+
+namespace StringCalculatorKataTestsX
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void GivenOneNumber_ShouldReturnThatNumber()
+        {
+            // Arrange
+            var sut = CreateStringCalculator();
+            var numbers = "1";
+            var expected = 1;
+
+            // Act
+            var actual = sut.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenManyNumber_ShouldReturnTheSumOfThoseNumbers()
+        {
+            // Arrange
+            var sut = CreateStringCalculator();
+            var numbers = "1,2";
+            var expected = 3;
+
+            // Act
+            var actual = sut.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(null, 0)]
+        [InlineData("", 0)]
+        [InlineData(" ", 0)]
+        [InlineData("     ", 0)]
+        public void GivenNoNumbers_ShouldReturn0(string numbers, int expected)
+        {
+            // Arrange
+            var sut = CreateStringCalculator();
+
+            // Act
+            var actual = sut.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenNewLineDelimiter_ShouldReturnTheSumOfAllNumbers()
+        {
+            // Arrange
+            var sut = CreateStringCalculator();
+            var numbers = "8\n13";
+            var expected = 21;
+
+            // Act
+            var actual = sut.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenCustomDelimiter_ShouldReturnTheSumOfAllNumbers()
+        {
+            // Arrange
+            var sut = CreateStringCalculator();
+            var numbers = "//;\n123;321";
+            var expected = 444;
+
+            // Act
+            var actual = sut.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GivenNegativeNumber_ShouldThrowException()
+        {
+            // Arrange
+            var sut = CreateStringCalculator();
+            var numbers = "//;\n123;-1";
+            var expected = "negatives not allowed -1";
+
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => sut.Add(numbers));
+            Assert.Equal(expected, exception.Message);
+        }
+
+        [Fact]
+        public void GivenCustomDelimiterOfAnyLength_ShouldReturnTheSumOfAllNumbers()
+        {
+            // Arrange
+            var sut = CreateStringCalculator();
+            var numbers = "//[^]\n10^11\n12";
+            var expected = 33;
+
+            // Act
+            var actual = sut.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        
+        [Theory]
+        [InlineData("//[^][%%]\n10^11%%12", 33)]
+        public void GivenMultipleCustomDelimitersOfAnyLength_ShouldReturnTheSumOfAllNumbers(string numbers,int expected)
+        {
+            // Arrange
+            var sut = CreateStringCalculator();
+           
+
+            // Act
+            var actual = sut.Add(numbers);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        private static StringCalculator CreateStringCalculator()
+        {
+            return new StringCalculator();
+        }
+    }
+}
---
+++ Example/StringCalculator.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        private readonly string[] _defaultDelimiters = {",", "\n"};
+        private const string _customDelimiterPrefix = "//";
+        private const char _customDelimiterSuffix = '\n';
+
+        public int Add(string numbers)
+        {
+            var parsedNumbers = ExtractNumbers(EmptyIfNull(numbers))
+                .Select(ParseNumber)
+                .ToList();
+
+            CheckForNegatives(parsedNumbers);
+
+            return parsedNumbers.Sum();
+        }
+
+        private IEnumerable<string> ExtractNumbers(string numbers)
+        {
+            return GetNumberSection(numbers)
+                .Split(GetDelimiters(numbers), StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private string[] GetDelimiters(string numbers)
+        {
+            var customDelimiters = GetDelimiterSection(numbers)
+                .Split(new []{"]", "["}, StringSplitOptions.RemoveEmptyEntries);
+
+            return _defaultDelimiters.Concat(customDelimiters)
+                .ToArray();
+        }
+
+        private string GetDelimiterSection(string numbers)
+        {
+            if (HasCustomDelimiter(numbers))
+            {
+                var start = _customDelimiterPrefix.Length;
+                var end = numbers.IndexOf(_customDelimiterSuffix, StringComparison.Ordinal);
+
+                return numbers.Substring(start, end - start);
+            }
+
+            return string.Empty;
+        }
+
+        private string GetNumberSection(string numbers)
+        {
+            if (HasCustomDelimiter(numbers))
+            {
+                var start = numbers.IndexOf(_customDelimiterSuffix, StringComparison.Ordinal);
+
+                return numbers.Substring(start, numbers.Length - start);
+            }
+
+            return numbers;
+        }
+
+        private bool HasCustomDelimiter(string numbers)
+        {
+            return numbers.StartsWith(_customDelimiterPrefix);
+        }
+
+        private string EmptyIfNull(string numbers)
+        {
+            return numbers ?? "";
+        }
+
+        private int ParseNumber(string numberAsText)
+        {
+            if (string.IsNullOrWhiteSpace(numberAsText))
+            {
+                return 0;
+            }
+
+            return int.Parse(numberAsText);
+        }
+
+        private void CheckForNegatives(IEnumerable<int> numbers)
+        {
+            var negativeNumbers = numbers.Where(number => number < 0).ToList();
+            if (negativeNumbers.Any())
+            {
+                throw new Exception($"negatives not allowed {string.Join(", ", negativeNumbers)}");
+            }
+        }
+    }
+}
